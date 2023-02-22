b80caf670159acf606bcbcb55324303980ab6e4e
skyblasterstudios <duon0065@algonquinlive.com>
2023-02-22 02:28:23 -0500

    Frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='.App-header'>
+        House of Wong Suite
+      </header>
+    </div>
+  );
+}
+
+export default App;
